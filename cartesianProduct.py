530a914b6c7a36fe843a681ec27ddf57e4df3563
Don Gagne <don@thegagnes.com>
2022-11-19 16:38:53 -0800

    Fixes

--- cartesianProduct.py
+++ cartesianProduct.py
@@ -1,3 +1,5 @@
+import matlab as ml
+
 import numpy as np
 
 def cartesianProduct(A: np.ndarray):
@@ -43,20 +45,20 @@ def cartesianProduct(A: np.ndarray):
     #     end
     # end
 
-    for i_col in range(num_sets):
+    for i_col in range(1, num_sets+1):
         # fill the column (aka fill the elements in the column)
         # stride is number of elements to repeat in a column
         # num_strides is number of strides per column
         stride      = num_members_per_set ** (num_sets - i_col)
         num_strides = num_rows_C // stride
-        i_col_A     = -1 # the column in A that we use
-        for i_stride in range(num_strides):
+        i_col_A     = 0 # the column in A that we use
+        for i_stride in range(1, num_strides+1):
             i_col_A = i_col_A + 1
-            if i_col_A > num_members_per_set - 1:
-                i_col_A = 0
+            if i_col_A > num_members_per_set:
+                i_col_A = 1
             star = (i_stride - 1) * stride + 1;
             # column of C gets elements from row of A
-            C[star : star + stride - 1, i_col] = A[i_col, i_col_A]
+            C[ml.siVector(star - 1, star + stride - 2), i_col - 1] = A[i_col-1, i_col_A-1]
 
     return C
 
